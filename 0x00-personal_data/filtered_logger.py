61ddbf4a9c8003e1c03166247675cc7831d40b59
Edmilson Cassecasse <>
2024-11-17 20:48:28 +0200

    fix: Reducing the length of the farmat method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -103,8 +103,6 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         """
         Formats a log record
-        :param record: The record to be formatted
-        :return: The formatted log record with some fields obfuscated.
         """
         msg = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
